f00004820ff98a4d7278bd5771b8405029773046
nirvik <nirivksaha@gatech.edu>
2019-04-24 16:01:36 -0400

    prepare for objects

--- public/js/utils/nsMain.js
+++ public/js/utils/nsMain.js
@@ -3,28 +3,36 @@ var scene3d=document.getElementById('Scene3d');
 var scene, camera, controls, renderer;
 
 var init= ()=>{
+    //prepare the scene
     scene=new THREE.Scene();
     scene.background=new THREE.Color("rgb(255,255,255)");
     
-    camera=new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000);
-    camera.up=new THREE.Vector3(0,0,1);
-    camera.position.x=0;
-    camera.position.y=10;
-    camera.position.z=10;
+    //prepare the camera
+    camera=new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 10000);
+    camera.position.set(500,500,1000);
+    camera.lookAt(0,0,0);
     
+    //prepare the webGL render
     renderer=new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     
+    //make the scene
     scene3d.appendChild(renderer.domElement);
 
-    var axes=new THREE.AxesHelper(5);
+    //axes and grids
+    var axes=new THREE.AxesHelper(150);
     scene.add(axes);
+    var gridHelper=new THREE.GridHelper(1000,20);
+    scene.add(gridHelper);
 
+    // camera orbit zoom
     controls=new THREE.OrbitControls(camera, renderer.domElement);
     controls.addEventListener('change', render);
     controls.enableZoom=true;
-    window.addEventListener('resize', onWindowResize, false);   
+
+    // window resize
+    window.addEventListener('resize', onWindowResize, false);  
 }
 
 var mainLoop=function(){
