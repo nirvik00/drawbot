013fcd9f6477a8670c63a27b251174b6e927ebbd
nirvik <nirivksaha@gatech.edu>
2019-04-28 04:20:01 -0400

    gui main menu complete

--- public/js/utils/nsMainSceneGraph.js
+++ public/js/utils/nsMainSceneGraph.js
@@ -1,20 +1,25 @@
 var CANVAS;//=document.getElementById("sceneGraphCanvas");
 var CTX;//=canvas.getContext("2d");
 
-var geoMenu, algMenu; // main menu
+var geo2dMenu, algMenu, aiMenu; // main menu
 
 window.onload=function(){
     CANVAS=document.getElementById("sceneGraphCanvas");
     CTX=CANVAS.getContext("2d");
-    geoMenu=new genGeoMainMenu(CTX);
+    
+    geo2dMenu=new genGeo2dMainMenu(CTX);
     algMenu=new genAlgMainMenu(CTX);
+    aiMenu=new aiMenu(CTX);
+
     draw();
 }
 
 function draw(){
     CTX.clearRect(0,0,CANVAS.width, CANVAS.height);
+
     algMenu.display();
-    geoMenu.display();
+    geo2dMenu.display();
+    aiMenu.display();
 
     CANVAS.addEventListener("mousedown", doMouseDown, false);
     CANVAS.addEventListener("mousemove", doMouseMove, false);
@@ -28,11 +33,18 @@ function doMouseDown(e){
     
     var t=algMenu.selected(x,y);
     if(t===true){
-        geoMenu.mousedown=false;
+        geo2dMenu.mousedown=false;
+        aiMenu.mousedown=false;
     }
-    var g=geoMenu.selected(x,y);
+    var g=geo2dMenu.selected(x,y);
     if(g===true){
         algMenu.mousedown=false;
+        aiMenu.mousedown=false;
+    }
+    var h=aiMenu.selected(x,y);
+    if(g===true){
+        algMenu.mousedown=false;
+        geo2dMenu.mousedown=false;
     }
 }
 
@@ -41,6 +53,6 @@ function doMouseMove(e){
     var y=e.pageY;    
 
     algMenu.hover(x,y);
-    geoMenu.hover(x,y);
-
+    geo2dMenu.hover(x,y);
+    aiMenu.hover(x,y);
 }
--- public/js/utils/sceneGraph/sgGeomComp.js
+++ public/js/utils/sceneGraph/sgGeomComp.js
@@ -1,5 +1,5 @@
 
-var genGeoMainMenu=function(ctx){
+var genGeo2dMainMenu=function(ctx){
     this.X=0;
     this.Y=0;
     this.L=120;
@@ -38,9 +38,11 @@ var genGeoMainMenu=function(ctx){
         this.ctx.fillStyle=this.colr;
         this.ctx.fillRect(this.X,this.Y,this.L,this.W);
         this.ctx.fillStyle="rgb(255,255,255)";
-        this.ctx.fillText("Geometry", 15, 30); 
+        this.ctx.fillText("Geometry", this.X+15, this.Y+30); 
     }
 }
+
+
 
 var genAlgMainMenu=function(ctx){
     this.X=130;
@@ -81,6 +83,50 @@ var genAlgMainMenu=function(ctx){
         this.ctx.fillStyle=this.colr;
         this.ctx.fillRect(this.X,this.Y,this.L,this.W);
         this.ctx.fillStyle="rgb(255,255,255)";
-        this.ctx.fillText("Algorithm", 150, 30); 
+        this.ctx.fillText("Algorithm", this.X+20, this.Y+30); 
+    }
+}
+
+
+var aiMenu=function(ctx){
+    this.X=270;
+    this.Y=0;
+    this.L=120;
+    this.W=50;
+    this.colr="rgb(0,0,0)";
+    this.ctx=ctx;
+    this.mouseover=false;
+    this.mousedown=false;
+
+    this.hover=function(x, y){
+        if(x>this.X+25 && x<this.X+this.L+25 && y>this.Y && y<this.Y+this.W+25){
+            this.mouseover=true;
+        }else{
+            this.mouseover=false;
+        }        
+    }
+    this.selected=function(x, y){
+        if(x>this.X+25 && x<this.X+this.L+25 && y>this.Y && y<this.Y+this.W+25){
+            this.mousedown=true;
+        }else{
+            this.mousedown=false;
+        }        
+    }
+    this.display=function(){
+        var hover=this.mouseover;
+        var sel=this.mousedown;        
+        if(hover===true && sel===false){
+            this.colr="rgb(155,50,50)";
+        }
+        else if(sel===true){
+            this.colr="rgb(255,0,0)";
+        }else{
+            this.colr="rgb(0,0,0)";
+        } 
+        this.ctx.font="20px Arial";
+        this.ctx.fillStyle=this.colr;
+        this.ctx.fillRect(this.X,this.Y,this.L,this.W);
+        this.ctx.fillStyle="rgb(255,255,255)";
+        this.ctx.fillText("AI", this.X+50, this.Y+30); 
     }
 }
